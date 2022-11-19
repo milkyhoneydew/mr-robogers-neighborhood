ea4c71dff87e28bb7836e52af2798d6f33fba393
Tanya Prado <tanyaprado@gmail.com>
2022-11-19 09:46:02 -0800

    comment out old functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,6 @@ function returnArray(num) {
   return numArray;
 }
 
-function beep(num) {
-  let array = returnArray(num);
-  let beepArray = array.map(function(element) {
-    if (element.toString().includes("1")) {
-      return "beep";
-    } else {
-      return element;
-    }
-  })
-  return beepArray;
-}
-
-function boop(num) {
-  let array = returnArray(num);
-  let boopArray = array.map(function(element) {
-    if (element.toString().includes("1") && !element.toString().includes("2")) {
-      return "beep";
-    } else if (element.toString().includes("2")) {
-      return "boop";
-    } else {
-      return element;
-    }
-  })
-  return boopArray;
-}
-
 function neighbor(num) {
   let array = returnArray(num);
   let neighborArray = array.map(function(element) {
@@ -47,3 +21,29 @@ function neighbor(num) {
   })
   return neighborArray;
 }
+
+// function beep(num) {
+//   let array = returnArray(num);
+//   let beepArray = array.map(function(element) {
+//     if (element.toString().includes("1")) {
+//       return "beep";
+//     } else {
+//       return element;
+//     }
+//   })
+//   return beepArray;
+// }
+
+// function boop(num) {
+//   let array = returnArray(num);
+//   let boopArray = array.map(function(element) {
+//     if (element.toString().includes("1") && !element.toString().includes("2")) {
+//       return "beep";
+//     } else if (element.toString().includes("2")) {
+//       return "boop";
+//     } else {
+//       return element;
+//     }
+//   })
+//   return boopArray;
+// }
